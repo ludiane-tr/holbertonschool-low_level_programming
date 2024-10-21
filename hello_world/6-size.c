05364b86a4720cfdec0319672dfb4ac6fa65001b
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-21 22:28:10 +0200

    new file

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+*prints the size of various types on the computer it is compiled and run on, follew by a new line.
+*/
+
+int main(void)
+
+{
+	printf("Size of a char :1 byte(s) \n", sizeof(char))
+	printf("Size of an int: 4 byte(s) \n", sizeof(int))
+	printf("Size of a long int: 4 byte(s) \n", sizeof(long int))
+	printf("Size of a long long int: 8 byte(s) \n", sizeof(long long))
+	printf("Size of a float: 4 byte(s) \n", sizeof(float))
+
+	return O;	
+}
