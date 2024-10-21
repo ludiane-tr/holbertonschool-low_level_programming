592e6d9d57d993ea57845e5543e0db44f39d05a4
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-21 14:50:46 +0200

    new file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle,followed by a new line.
+ * main - prints "Programming is like building a multilingual
+ * 		  puzzle,followed by a new line.
+ *
  * Return: Always O.
- * /
+ */
 
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
+
 	return (0);
 }
