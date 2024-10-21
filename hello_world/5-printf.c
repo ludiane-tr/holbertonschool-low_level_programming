69e686d07b9245965cda2ec27ec8b04b181ebe41
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-21 15:17:51 +0200

    new file

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of 
+ * art, ,followed by a new line.
+ *
+ * Return: Always O.
+ */
+
+int main(void)
+{
+        printf("\"with proper grammar, but the outcome is a piece of art");
+
+        return (0);
+}
