54f588f8e45b491660e62ea0c1d2b43df2a1d4ef
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-21 14:54:07 +0200

    new file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * 		  puzzle,followed by a new line.
+ * 		puzzle,followed by a new line.
  *
  * Return: Always O.
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
