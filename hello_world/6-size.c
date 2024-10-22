e791e46ba5a0235684967f1e917b7d782727aa59
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-22 09:19:52 +0200

    new file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /*
-*prints the size of various types on the computer it is compiled and run on, follew by a new line.
+* Déclaration et affectation d'une variable de type entier (int) .
+* char = 1
 */
 
 int main(void)
 
 {
-	printf("Size of a char :1 byte(s) \n", sizeof(char))
+	printf("Size of a char: 1 byte(s) \n", sizeof(char))
 	printf("Size of an int: 4 byte(s) \n", sizeof(int))
 	printf("Size of a long int: 4 byte(s) \n", sizeof(long int))
 	printf("Size of a long long int: 8 byte(s) \n", sizeof(long long))
