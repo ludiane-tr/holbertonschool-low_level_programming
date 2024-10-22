e42b867461a3ea0b13ad65facc68cd672f83ca29
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-22 15:12:07 +0200

    new file

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
 * main - assign a random number to the variable n.
 * betty style doc for function main goes there.
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
