9f96ab342604bed8f4242e42d900e09b13786172
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-22 10:14:42 +0200

    new file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-/*
-* Déclaration et affectation d'une variable de type entier (int) .
-* char = 1
+/**
+* main - prints the size of various types on the computer it is compiled and run on
+* Return: Always O.
 */
 
 int main(void)
 
 {
-	printf("Size of a char: 1 byte(s) \n", sizeof(char))
-	printf("Size of an int: 4 byte(s) \n", sizeof(int))
-	printf("Size of a long int: 4 byte(s) \n", sizeof(long int))
-	printf("Size of a long long int: 8 byte(s) \n", sizeof(long long))
-	printf("Size of a float: 4 byte(s) \n", sizeof(float))
+	printf("Size of a char = %lu byte(s) \n", sizeof(char)*1 );
+	printf("Size of an int = %lu byte(s) \n", sizeof(int)*4 );
+	printf("Size of a long int = %lu byte(s) \n", sizeof(long int)*8 );
+	printf("Size of a long long int = %lu byte(s) \n", sizeof(long long int)*8 );
+	printf("Size of a float = %lu byte(s) \n", sizeof(float)*4 );
 
-	return O;	
+	return (O);
 }
