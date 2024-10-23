a6cd799e13b6b1ba8cdf8d5f803586c5197c109a
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-23 11:06:37 +0200

    new file

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
-
 /**
 * main - prints the alphabet in lowercase
 * Return: 0
 */
 int main(void)
 {
-	char alphabet;
+	char letter;
 
-	for (alphabet = 'a'; alphabet <= 'z')
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		putchar(alphabet);
+		putchar(letter);
 	}
 	putchar('\n');
+
 	return (0);
 }
