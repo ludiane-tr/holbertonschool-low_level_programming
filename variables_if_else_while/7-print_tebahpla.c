96d69d7ce57e1995e6d3f7a52d0ce8194c752280
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-23 12:31:24 +0200

    new file

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet in lowercase in reverse.
+* Return: 0
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
