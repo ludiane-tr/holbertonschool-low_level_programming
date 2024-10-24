4eb137e699a62b67a2502f41bf802382ba73c9e6
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-24 21:50:09 +0200

    new file

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
-* print_alphabet - prints the alphabet in lowercase, followed by a new line.
+* main - check the code
+* Return: Always 0.
 */
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+		_putchar.c(letter);
 
-	_putchar('\n');
+	_putchar.c('\n');
+	return 0
 }
