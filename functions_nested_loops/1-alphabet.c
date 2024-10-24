73daed1ce1a2d397e3606e71f079a4b1ba397b14
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-24 22:23:42 +0200

    new file

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
-#include "main.h"
+#include"main.h"
 /**
 * main - check the code
-* Return: Always 0.
 */
 void print_alphabet(void);
 {
-	char letter;
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 
 	_putchar('\n');
 }
