e1f360d579d6f65da20a191aeee9b7512c5f8b86
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-25 12:48:11 +0200

    new file

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/*
+ * _isalpha- checks for alphabetic character.
+ * @r: parameter
+ * Return:0
+ */
+int _isalpha(int r)
+{
+	if ((r >= 'a' && r <= 'z') ||
+	    (r >= 'A' && r <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
