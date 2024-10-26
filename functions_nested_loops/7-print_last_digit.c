6f92c4dc55f985ca35f175435ffc56d28ed14e12
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-26 22:56:28 +0200

    new file

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include"main.h"
 /**
 * print_last_digit - Prints the last digit of a number.
@@ -7,12 +8,10 @@
 int print_last_digit(int)
 {
 	int last_digit = n % 10;
-	{
-		if (last_digit < 0)
-			last_digit *= -1;
-	}
-	_putchar(last_digit + '0');
+	
+	if (last_digit < 0)
+			last_digit *= -last_digit;
+	_putchar('0' + last_digit);
 
 	return (last_digit);
 }
-
