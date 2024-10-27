4c1ff35af32eb58fed31f18ac20a0499cf1c2361
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-27 20:22:32 +0100

    new file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+* add - Write a function that adds two integers.
+* @num1: The first integer
+* @num2: The second integer
+* Return: the result of the addition.
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
