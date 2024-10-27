9303ff792d861f7bdc94b57d4fc72f42696ac94a
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-27 16:19:05 +0100

    new file

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,10 @@
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	
-	if (last_digit < 0)
-			last_digit *= -last_digit;
+	{
+		if (last_digit < 0)
+			last_digit *= -1;
+	}
 	_putchar('0' + last_digit);
-
 	return (last_digit);
 }
