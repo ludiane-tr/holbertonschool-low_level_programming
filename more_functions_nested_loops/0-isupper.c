d7d5d38b1a914cc68cdb7f2f7b6f2d4ebdbb6479
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-28 10:39:27 +0100

    new file

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * isupper - checks for uppercase character.
+ * @c: parameters
+ * Return:0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+}
