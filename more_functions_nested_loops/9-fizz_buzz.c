9780ebed3d0f387c2bafee151a313f985e86d8d9
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-29 15:46:33 +0100

    new file

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ * main - prints the numbers from 1 to 100.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("Fizz Buzz ");
+		}
+		else if (i == 100)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
