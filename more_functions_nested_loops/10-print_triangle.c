5de5dc2be4dda489e31e568fc74379a0028c1d78
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-30 10:50:22 +0100

    new file

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,23 +6,27 @@
 void print_triangle(int size)
 {
 	int i, j;
+
 	if (size > 0)
 	{
-		for (i = 0 ; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (i = 0 ; i <= size; i--)
+			for (j = 1; j <= size; j++)
 			{
-				_putchar(' ');
+				if (j <= (size - i))
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
 			}
-			for (j = 0 ; j <= size ; j++)
-			{
-				_putchar('#');
-			}
-
-			if (i == size)
-
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else if (size <= 0)
+	{
+		_putchar('\n');
+	}
 }
--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -7,9 +7,12 @@
  */
 int main(void)
 {
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    print_triangle(3);
+    print_triangle(9);
+    print_triangle(98);
     return (0);
 }
