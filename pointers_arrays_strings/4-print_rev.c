7a08290efa8feb534ca50649c890a74770a07d30
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-31 15:32:49 +0100

    new file

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,17 @@
 void print_rev(char *s)
 {
 	int length = 0;
+	int i;
 	
 	while (s[length] != '\0')
-    {
-	    length++;
-    }
-	for (int i = length - 1; i >= 0; i--)
-    {
-	    putchar(s[i]);
-    }
+	{
+		length++;
+	}
+	{
+		for (int i = length - 1; i >= 0; i--)
+		{
+			putchar(s[i]);
+		}
+	}
 	_putchar('\n');
 }
