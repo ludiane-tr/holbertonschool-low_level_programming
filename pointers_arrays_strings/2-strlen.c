def7068234e6bbe82e47926ab26c4a2358616fd6
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-31 13:24:52 +0100

    new file

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strlen - returns the length of a string.
- * @s:The string to get the length of.
- */
+* _strlen - returns the length of a string.
+* @s:The string to get the length of.
+*/
 int _strlen(char *s)
 {
 	int len = 0;
