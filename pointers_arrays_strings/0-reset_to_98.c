f76ed66499cb3931df10218ab683c67bc767a190
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-10-31 09:00:00 +0100

    new file

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter 
- * @n: parameters
- */
+* reset_to_98 - takes a pointer to an int as parameter
+* @n: parameters
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
