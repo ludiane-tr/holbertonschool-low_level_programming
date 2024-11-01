c16cecfc0f7bc9546dec7e79d45428363960e1dc
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-01 18:50:16 +0100

    new file

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * Print_array - 
+ */
+
+
+
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}
