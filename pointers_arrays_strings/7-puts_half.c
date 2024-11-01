6c37bc24b321a107390082c0758509f9a6599223
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-01 23:08:00 +0100

    new file

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,13 +10,10 @@ void puts_half(char *str)
 
 	while (str[index++])
 		len++;
-	
 	if ((len % 2) == 0)
 		n = len / 2;
-	
 	else
 		n = (len + 1) / 2;
-	
 	for (index = n; index < len; index++)
 		_putchar(str[index]);
 	
