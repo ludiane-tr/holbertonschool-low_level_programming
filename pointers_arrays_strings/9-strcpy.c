e6023c31e6af60b3aad63f35eb9b37f41f569891
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-02 21:30:17 +0100

    new file

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - Copies the string pointed to by @src.
+ * @dest: parameters
+ * @src: parameters
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
