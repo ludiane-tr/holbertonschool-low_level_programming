b38bbd287f62c04f5d29987afcb4116f1ac13d9e
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-03 10:49:43 +0100

    new file

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-        int index = 0;
+	int index = 0, dest_len = 0;
 
-        while (src[index])
-        {
-                dest[index] = src[index];
-                index++;
-        }
-        dest[index] = '\0';
+	while (dest[index++])
+		dest_len++;
 
-        return (dest);
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
