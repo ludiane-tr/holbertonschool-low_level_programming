3583ba831f6b642bc27759367009f6984f4f07d0
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-03 22:07:42 +0100

    new file

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int index = 0, dest_len = 0;// dest len est une variable qui stocke la longueur de dest qui est l'endroit où src sera ajouté
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[index++])// récupère la taille de dest et parcoure chaque caractère avec l'index
+		dest_len++;// la boucle continue tant que le caractère nul n'est pas atteint.
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	for (index = 0; src[index]; index++)//récupérer la taille de src
+		dest[dest_len++] = src[index];// copier les caractères de src dans dest à partir de la position dest_len
 	return (dest);
 }
