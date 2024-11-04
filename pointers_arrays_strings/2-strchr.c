9925881a1fd9af9aa45b43406f3f9ab7dc05d0ed
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-04 15:59:53 +0100

    new file

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - locates a character in a string
+ * @c: parameters
+ * @s: parameters
+ * Return: pointer c in the string s.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++);
+
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
+}
