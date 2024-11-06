636ccf3f3fe5655805297e369f8c01d4b5e823c6
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-06 09:53:57 +0100

    new file

---
+++ pointers_arrays_strings/1-test.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *                                                                   * Return: Always 0.
+ */                                                                 int main(void)
+{
+        int i;
+        
+	for (i = 0 ; i < 5 ; i+2)
+	{
+		printf("Thierry répond au téléphone !\n");
+	}
+
+	return 0;
+}   
