b410039fd3b8b8f7e7811dea50278829626f47b5
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-08 16:56:33 +0100

    Update 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: parameters
- * @argv: parameters
+ * main - Entry point
  *
- * Return: Always o.
+ * @argc: Number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
-
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
