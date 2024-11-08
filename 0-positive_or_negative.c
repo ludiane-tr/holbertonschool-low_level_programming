374c5a188f187e351165fcfd0a4855663e53f214
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-08 10:13:38 +0100

    new file

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	int n;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+
+	return(0);
+}
