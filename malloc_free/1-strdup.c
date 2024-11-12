93ca2b17e63e8c646f8266f3cf3eb0a4508a952f
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-12 14:02:27 +0100

    new file

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_strdup - write a function that returns a pointer
+ * @*str: parameters
+ * Return: a pointer
+ */
+char *_strdup(char *str)
+{
+	int l, i;
+	char *s;
+
+	if (str == NULL)
+		return (0);
+
+	l = 0;
+	while (*(str + l))
+		l++;
+
+	s = malloc(sizeof(char) * l + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i <= l; i++)
+	{
+		*(s + i) = *(str + i);
+	}
+	return (s);
+}
