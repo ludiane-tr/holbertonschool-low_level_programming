fccb203f0ba5329d8ebdcd76e029d1c33c0d0d9d
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-13 21:01:18 +0100

    new file

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-int main(void);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
