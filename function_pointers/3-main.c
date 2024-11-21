b3c299a0691dcd5790c1ce475dba2b1fc74ecae3
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-11-21 22:25:30 +0100

    new file

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,11 +4,11 @@
  * main - program to perform simple operations
  * @argc: argument count
  * @argv: array of arguments
- * Return: exits 0 normally, 98 on argc error, 99 on illegal operator
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, result;
+	int num1, num2, result;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -17,10 +17,10 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && n2 == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	func = get_op_func(argv[2]);
-	result = func(n1, n2);
+	result = func(num1, num2);
 
 	printf("%d\n", result);
 	return (0);
