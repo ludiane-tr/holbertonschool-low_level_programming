b90cc0de20e40d801c78863a1659a200f49b1309
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-12-04 10:07:59 +0100

    clean the repo

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,11 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 /*Prototypes*/
