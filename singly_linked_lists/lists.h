8baad1c69aebee1e53514082f4a53cb697cfb21f
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-12-04 10:02:59 +0100

    write a function that prints all the elements

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/*Prototypes*/
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
