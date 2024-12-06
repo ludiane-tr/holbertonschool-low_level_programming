4e5e1e1f652956a478d7478a7028bd2db0412004
ludiane-tr <ludiane.trouillefou@gmail.com>
2024-12-06 09:55:23 +0100

    insert node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -7,35 +7,32 @@
  * @n: data (n) of the node at index
  *Return:  the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *prev_node;
-	dlistint_t *new_node;
-	prev_node = *h;
+	unsigned int i = 0;
+	dlistint_t *prev_node = *h;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
+
 	while (i < (idx - 1))
 	{
 		if (!prev_node)
-		{
 			return (NULL);
-		}
-	prev_node = prev_node->next;
-	i++;
+
+		prev_node = prev_node->next;
+		i++;
 	}
+
 	if (prev_node->next == NULL)
 		return (add_dnodeint_end(h, n));
-
-	new_node = malloc(sizeof(dlistint_t));
 
 	new_node->n = n;
 	new_node->next = prev_node->next;
 	new_node->prev = prev_node;
 	prev_node->next->prev = new_node;
 	prev_node->next = new_node;
+
 	return (new_node);
 }
